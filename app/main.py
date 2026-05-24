327e9c26f2f058ca92f644e498939f0a441aa058
vitaliyoliynik <oliynikvs@gmail.com>
2026-05-24 17:00:28 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -15,8 +15,10 @@ def create_person_list(people: list) -> list:
 
     for person_dict in people:
         current_person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            current_person.wife = Person.people[person_dict["wife"]]
+        spouse_name = person_dict.get("wife") or person_dict.get("husband")
+        if spouse_name:
+            spouse = Person.people[spouse_name]
+            current_person.wife = spouse
             current_person.wife.husband = current_person
 
     return result
