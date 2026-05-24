77f8245d61430a0124cb358e7d4b7497fc9a509b
vitaliyoliynik <oliynikvs@gmail.com>
2026-05-24 16:49:00 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,3 @@
-class Person:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        Person.people[name] = self
-
-
 def create_person_list(people: list) -> list:
     result = [
         Person(person_dict["name"], person_dict["age"])
@@ -17,5 +8,6 @@ def create_person_list(people: list) -> list:
         current_person = Person.people[person_dict["name"]]
         if "wife" in person_dict and person_dict["wife"] is not None:
             current_person.wife = Person.people[person_dict["wife"]]
+            current_person.wife.husband = current_person
 
     return result
